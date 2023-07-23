3d75fd1728f6b87f4d32c095ca66b9f0f92ac798
vickyadri29 <vickyadri290501@gmail.com>
2023-07-23 16:19:03 +0800

    feat: add animation on burstbload

--- src/components/About/BurstBload.tsx
+++ src/components/About/BurstBload.tsx
@@ -3,9 +3,11 @@ import burstBloadIcons from "/icons/burst-bloat.svg";
 
 const BurstBload: React.FC = () => {
   return (
-    <div style={{ overflow: "scroll" }}>
+    <div>
       <motion.img
         src={burstBloadIcons}
+        animate={{ scale: [0.8, 0.8, 1.05], rotate: [0, 360] }}
+        transition={{ ease: 'easeInOut', repeatType: "reverse", repeat: Infinity, duration: 1.2 }}
         alt="about shape vickyadrii"
         className="absolute -z-10 top-0 md:right-4 right-5 md:w-auto w-6"
       />
