ad216593ef331acdc558488a948fdbc7c38fc67d
vickyadri29 <vickyadri290501@gmail.com>
2023-07-24 03:16:16 +0800

    fsdfds

--- src/components/Headers/Navbar.tsx
+++ src/components/Headers/Navbar.tsx
@@ -1,7 +1,7 @@
-import { Fragment, useState } from "react";
+import { Fragment } from "react";
 import { dataNavbar } from "../../data/dummyData";
 import { motion } from "framer-motion";
-import { Link } from "react-scroll";
+// import type { Link } from "react-scroll";
 
 const Navbar: React.FC = () => {
   const containerNavbar = {
@@ -17,28 +17,24 @@ const Navbar: React.FC = () => {
 
   return (
     <Fragment>
-      <motion.nav
+      <motion.div
         className="flex justify-center top-0 fixed left-1/2 z-50 -translate-x-1/2 my-5"
         variants={containerNavbar}
         initial="hidden"
         animate="visible"
       >
-        <motion.ul className="xl:flex hidden items-center gap-24 px-20 py-5 bg-white/30 border backdrop-blur-sm drop-shadow-sm hover:drop-shadow-lg transition-all duration-200 rounded-full">
+        <motion.div className="xl:flex hidden items-center gap-24 px-20 py-5 bg-white/30 border backdrop-blur-sm drop-shadow-sm hover:drop-shadow-lg transition-all duration-200 rounded-full">
           {dataNavbar.map(({ id, navigate, navigate_url }) => (
-            <Link
+            <a
+              href={navigate_url}
               key={id}
-              to={navigate_url}
-              smooth={true}
-              duration={450}
-              offset={-100}
-              spy={true}
               className="text-primary opacity-30 hover:opacity-80 font-semibold cursor-pointer"
             >
               {navigate}
-            </Link>
+            </a>
           ))}
-        </motion.ul>
-      </motion.nav>
+        </motion.div>
+      </motion.div>
     </Fragment>
   );
 };
