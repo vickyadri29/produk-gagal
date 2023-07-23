8aa16469544d89f91e33e42a89c888e94f499177
vickyadri29 <vickyadri290501@gmail.com>
2023-07-24 03:10:08 +0800

    fsdfds

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,11 +1,1 @@
-// /// <reference types="astro/client" />
-// declare global {
-//   var myString: string;
-//   function myFunction(): boolean;
-// }
-
-// interface Window {
-//   myFunction: boolean;
-// }
-
-// export {};
+/// <reference types="astro/client" />
